d2ebef008cf774e9ef15475d3ba4025814ecbb81
mshimabukuro <mathiassht@gmail.com>
2024-07-18 10:33:51 -0300

    trabajo final incompleto

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { paginate } from "mongoose-paginate-v2";
 
 const cartSchema = new mongoose.Schema ({
     products: [
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -41,11 +41,25 @@ router.post("/:cid/product/:pid", async (req, res) => {
         res.status(500).json({ error: "Error" });
     }
 });
+router.delete("/:cid", async (req, res) => {
+    const cartId = req.params.cid;
+    try {
+        const carrito = await CartModel.findByIdAndDelete(cartId)
+        res.json({
+            message: "Carrito vaciado exitosamente"
+        });
+    } catch (error) {
+        console.error("No se pudo eliminar el carrito", error);
+        res.status(500).json({
+            error: "Error de servidor"
+        });
+    }
+});
 router.delete("/:cid/product/:pid", async (req, res) => {
     const cartId = req.params.cid;
     const productId = req.params.pid;
     try {
-        const carrito = await CartModel.findByIdAndDelete(req.params.cid, req.params.pid)
+        const carrito = await CartModel.findByIdAndDelete(cartId, productId)
         res.json({
             message: "Producto eliminado exitosamente"
         });
@@ -55,6 +69,35 @@ router.delete("/:cid/product/:pid", async (req, res) => {
             error: "Error de servidor"
         });
     }
+});
+
+router.put("/:cid", async (req, res) => {
+    try {
+        const carrito = await CartModel.findByIdAndUpdate(cartId);
+        if (!carrito) {
+            return res.status(404).send("No se pudo modificar el carrito")
+        } else {
+            res.status(200).send("Carrito modificado con exito")
+        }
+    } catch (error) {
+        res.status(500).send("Error del servidor")
+    }
+
+});
+
+router.put("/:cid/product/:pid", async (req, res) => {
+    const quantityId = req.params.quantity
+    try {
+        const carrito = await CartModel.findByIdAndUpdate(quantityId);
+        if (!carrito) {
+            return res.status(404).send("Solo se puede modificar la cantidad")
+        } else {
+            res.status(200).send("Cantidad modificada correctamente")
+        }
+    } catch (error) {
+        res.status(500).send("Error del servidor")
+    }
+
 });
 
 export default router;
